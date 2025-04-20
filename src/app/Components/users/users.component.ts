4041e81013f7d2be9268704f63cae4a2e703ce57
AhmedHussein222 <ahmdhussein222@gmail.com>
2025-04-20 08:51:34 +0200

    add user component and connect to firebase

---
+++ src/app/Components/users/users.component.ts
@@ -0,0 +1,27 @@
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UsersService } from '../../Services/users.service';
+import { Iuser } from '../../Models/iuser/iuser.module';
+
+@Component({
+  selector: 'app-users',
+  imports: [CommonModule],
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.scss'],
+})
+export class UsersComponent implements OnInit {
+  private firestore = inject(UsersService);
+  users: Observable<Iuser[]>;
+
+  constructor() {
+    this.users = this.firestore.getAll('Users');
+    console.log(this.users);
+  }
+
+  ngOnInit(): void {}
+
+  trackByEmail(index: number, user: any): string {
+    return user.email;
+  }
+}
---
+++ src/app/Models/iuser/iuser.module.ts
@@ -0,0 +1,15 @@
+export interface Iuser {
+  first_name: string;
+  last_name: string;
+  email: string;
+  gender: string;
+  bio: string;
+  role: string;
+  createAt: Date;
+  links?: {
+    linkedin?: string;
+    facebook?: string;
+    youtube?: string;
+    instagram?: string;
+  }
+}
