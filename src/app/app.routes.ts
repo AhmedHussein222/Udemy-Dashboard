59bc8165d8377c1fd031f95030e5f004cd0c3d52
AhmedHussein222 <ahmdhussein222@gmail.com>
2025-04-20 08:52:45 +0200

    create routing for component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,13 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './Components/main/main.component';
+import { UsersComponent } from './Components/users/users.component';
 
 export const routes: Routes = [
+  {path: '', redirectTo: 'main', pathMatch: 'full'},
+  {path: 'main', component :MainComponent ,children:
+    [
+      {path:'',component:UsersComponent},
+      
+    ]
+  }
 ];
