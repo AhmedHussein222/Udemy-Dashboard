03571e5af5a34ee64616c47005de49c7da276b9a
AhmedHussein222 <ahmdhussein222@gmail.com>
2025-04-20 12:50:24 +0200

    update chart summaries in  user component

--- src/app/Components/users/users.component.ts
+++ src/app/Components/users/users.component.ts
@@ -1,24 +1,38 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Chart, ChartModule } from 'angular-highcharts';
+import { Iuser } from '../../Models/iuser/iuser';
 import { UsersService } from '../../Services/users.service';
-import { Iuser } from '../../Models/iuser/iuser.module';
-
 @Component({
   selector: 'app-users',
-  imports: [CommonModule],
+  imports: [ ChartModule],
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss'],
 })
 export class UsersComponent implements OnInit {
   private firestore = inject(UsersService);
-  users: Observable<Iuser[]>;
+  users!: Iuser[];
 
   constructor() {
-    this.users = this.firestore.getAll('Users');
-    console.log(this.users);
-  }
+    // this.users = this.firestore.getAll('Users')
+    this.firestore.getAll('Users').subscribe((data) => {
+      this.users = data;
+    });
 
+  }
+  testchart = new Chart({
+    title: { text: 'Users by Role' },
+    series: [
+      {
+        type: 'pie',
+
+        data: [
+          { name: 'Student', y: 60, color: '#483D8B' },
+          { name: 'instructors', y: 20, color: '#7B68EE' },
+        ],
+      },
+    ],
+  });
   ngOnInit(): void {}
 
   trackByEmail(index: number, user: any): string {
--- src/app/Services/users.service.ts
+++ src/app/Services/users.service.ts
@@ -1,24 +1,25 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
+import { Iuser } from '../Models/iuser/iuser';
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
   constructor(private firestore : AngularFirestore ) { }
-  getAll(collectionName: string): Observable<any[]> {
-    return this.firestore.collection(collectionName).valueChanges();
+  getAll(collectionName: string): Observable<Iuser[]> {
+    return this.firestore.collection<Iuser>(collectionName).valueChanges();
   }
-  getOne(collectionName: string, id: string): Observable<any> {
-    return this.firestore.collection(collectionName).doc(id).valueChanges();
+  getOne(collectionName: string, id: string): Observable<Iuser> {
+    return this.firestore.collection<Iuser>(collectionName).doc(id).valueChanges() as Observable<Iuser>;
   }
 
   // جلب بيانات مع استعلام
-  query(collectionName: string, field: string, condition: any, value: any): Observable<any[]> {
-    return this.firestore.collection(collectionName, ref => 
-      ref.where(field, condition, value)
-    ).valueChanges({ idField: 'id' });
-  }
+  // query(collectionName: string, field: string, condition: any, value: any): Observable<any[]> {
+  //   return this.firestore.collection(collectionName, ref => 
+  //     ref.where(field, condition, value)
+  //   ).valueChanges({ idField: 'id' });
+  // }
 
 }
