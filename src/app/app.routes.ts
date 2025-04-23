2ea446f6d141f3f93ba36ab069f3ad887d76182d
AhmedHussein222 <ahmdhussein222@gmail.com>
2025-04-23 20:59:47 +0200

    Update routeing with new components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,19 @@
 import { Routes } from '@angular/router';
-import { MainComponent } from './Components/main/main.component';
-import { UsersComponent } from './Components/users/users.component';
 import { DashboardComponent } from './Components/dashboard/dashboard.component';
+import { MainComponent } from './Components/main/main.component';
+import { NotFoundComponent } from './Components/not-found/not-found.component';
+import { UsersComponent } from './Components/users/users.component';
 
 export const routes: Routes = [
-  {path: '', redirectTo: 'main', pathMatch: 'full'},
-  {path: 'main', component :MainComponent ,children:
-    [
-      {path:'',component:UsersComponent},
-      {path:'Dashboard',component:DashboardComponent},
-      
-    ]
-  }
+  { path: '', redirectTo: 'main', pathMatch: 'full' },
+  {
+    path: 'main',
+    component: MainComponent,
+    children: [
+      { path: '', redirectTo: 'Dashboard', pathMatch: 'full' },
+      { path: 'Dashboard', component: DashboardComponent },
+      { path: 'Users', component: UsersComponent },
+    ],
+  },
+  { path: '**', component: NotFoundComponent },
 ];
