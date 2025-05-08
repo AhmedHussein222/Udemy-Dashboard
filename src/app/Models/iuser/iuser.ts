5c8867cabfc78e6ebabd719812bed8025be78f00
AhmedHussein222 <ahmdhussein222@gmail.com>
2025-05-08 13:41:16 +0300

    add crud operation to user component

--- src/app/Models/iuser/iuser.ts
+++ src/app/Models/iuser/iuser.ts
@@ -1,16 +1,18 @@
 export interface Iuser {
+  user_id: string;
   first_name: string;
   last_name: string;
   email: string;
+  password: string;
   gender: string;
   bio: string;
   role: string;
-  createAt: Date;
+  created_at: Date;
   profile_picture: string;
   links?: {
     linkedin?: string;
     facebook?: string;
     youtube?: string;
     instagram?: string;
-  }
+  };
 }
--- src/app/Services/users.service.ts
+++ src/app/Services/users.service.ts
@@ -3,24 +3,45 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
 import { Iuser } from '../Models/iuser/iuser';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UsersService {
-
-  constructor(private firestore : AngularFirestore ) { }
-  getAll(collectionName: string): Observable<Iuser[]> {
-    return this.firestore.collection<Iuser>(collectionName).valueChanges();
+  private readonly collectionName = 'Users';
+  constructor(private firestore: AngularFirestore) {}
+  getAll(): Observable<Iuser[]> {
+    return this.firestore
+      .collection<Iuser>(this.collectionName)
+      .valueChanges({ idField: 'user_id' }); // Add idField to include document ID
   }
 
-  getOne(collectionName: string, id: string): Observable<Iuser> {
-    return this.firestore.collection<Iuser>(collectionName).doc(id).valueChanges() as Observable<Iuser>;
+  getOne(id: string): Observable<Iuser> {
+    return this.firestore
+      .collection<Iuser>(this.collectionName)
+      .doc(id)
+      .valueChanges() as Observable<Iuser>;
+  }
+  updateUser(id: string, data: Partial<Iuser>): Promise<void> {
+    return this.firestore
+      .collection<Iuser>(this.collectionName)
+      .doc(id)
+      .update({ ...data });
+  }
+  deleteUser(id: string): Promise<void> {
+    return this.firestore
+      .collection<Iuser>(this.collectionName)
+      .doc(id)
+      .delete();
   }
 
   // جلب بيانات مع استعلام
-  query(collectionName: string, field: string, condition: any, value: any): Observable<any[]> {
-    return this.firestore.collection(collectionName, ref => 
-      ref.where(field, condition, value)
-    ).valueChanges({ idField: 'id' });
+  query(
+    collectionName: string,
+    field: string,
+    condition: any,
+    value: any
+  ): Observable<any[]> {
+    return this.firestore
+      .collection(collectionName, (ref) => ref.where(field, condition, value))
+      .valueChanges({ idField: 'id' });
   }
-
 }
